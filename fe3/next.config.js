fbf0ab01afbc3a1c34c1483facde3db34b4b7e35
KZ-Lucas <qq3884qq@naver.com>
2022-11-21 22:39:42 +0900

    nextjs microfrontend 구현

--- fe3/next.config.js
+++ fe3/next.config.js
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
+const path = require("path");
 const nextConfig = {
   reactStrictMode: true,
   webpack5: true,
   webpack: (config, options) => {
     config.plugins.push(
       new options.webpack.container.ModuleFederationPlugin({
-        name:"fe3",
-        filename: './consumerFile.js',
+        name: "fe3",
+        filename: "./consumerFile.js",
         remotes: {
-            fe1: 'fe1@http://localhost:8888/remoteEntry.js',
-            fe2: 'fe2@http:localhost:8888/remoteEntry2.js',
+          fe1: "fe1@http://localhost:3000/remoteEntry.js",
         },
         shared: [
           {
@@ -18,20 +17,20 @@ const nextConfig = {
               eager: true,
               singleton: true,
               requiredVersion: false,
-            }
+            },
           },
           {
             "react-dom": {
               eager: true,
               singleton: true,
               requiredVersion: false,
-            }
+            },
           },
-        ]
+        ],
       })
-    )
-    return config
-  }
-}
+    );
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- fe3/pages/index.js
+++ fe3/pages/index.js
@@ -1,20 +1,17 @@
-import React from 'react'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import React from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
-const Header = React.lazy( () => import('fe1/header'));
-const squareRoot = React.lazy( () => import('fe2/getSquareRoot'));
+const Header = React.lazy(() => import("fe1/header"));
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
       <main className={styles.main}>
-        <h1 className={styles.title}>
-           Square root of 4: {squareRoot(4)}
-        </h1>
+        <h1 className={styles.title}>Square root of 4:</h1>
       </main>
     </div>
-  )
+  );
 }
